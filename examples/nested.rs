e5d38adb674f8295626030f32edaf751e4c71587
Duncan Freeman <duncan.freeman1@gmail.com>
2024-03-17 21:38:51 -0700

    Easy interpolate cube

--- examples/nested.rs
+++ examples/nested.rs
@@ -7,6 +7,7 @@ use splines::{Interpolation, Key, Spline, interpolate::NestedSpline};
 fn main() {
   let interp = Interpolation::Linear;
 
+
   let keys = vec![
     Key::new(0.0f32, 0.0f32, interp),
     Key::new(1.0f32, 1.0f32, interp),
@@ -19,14 +20,47 @@ fn main() {
   ];
   let spline_b = Spline::from_vec(keys);
 
-  let nested = Spline::from_vec(vec![
+  let nested_a = Spline::from_vec(vec![
     Key::new(0.0f32, NestedSpline::Unit(Rc::new(spline_a)), Interpolation::Linear),
     Key::new(1.0f32, NestedSpline::Unit(Rc::new(spline_b)), Interpolation::Linear),
   ]);
 
-  println!("value at 0, 0: {:?}", dbg!(nested.sample(0.1)).unwrap().sample(0.1));
-  println!("value at 0, 1: {:?}", nested.sample(0.1).unwrap().sample(0.9));
-  println!("value at 1, 0: {:?}", nested.sample(0.9).unwrap().sample(0.1));
-  println!("value at 1, 1: {:?}", nested.sample(0.9).unwrap().sample(0.9));
+
+
+  let keys = vec![
+    Key::new(0.0f32, 0.0f32, interp),
+    Key::new(1.0f32, 1.0f32, interp),
+  ];
+  let spline_a = Spline::from_vec(keys);
+
+  let keys = vec![
+    Key::new(0.0f32, -100.0f32, interp),
+    Key::new(1.0f32, 100.0f32, interp),
+  ];
+  let spline_b = Spline::from_vec(keys);
+
+  let nested_b = Spline::from_vec(vec![
+    Key::new(0.0f32, NestedSpline::Unit(Rc::new(spline_a)), Interpolation::Linear),
+    Key::new(1.0f32, NestedSpline::Unit(Rc::new(spline_b)), Interpolation::Linear),
+  ]);
+
+
+
+  let nested = Spline::from_vec(vec![
+    Key::new(0.0f32, NestedSpline::Unit(Rc::new(nested_a)), Interpolation::Linear),
+    Key::new(1.0f32, NestedSpline::Unit(Rc::new(nested_b)), Interpolation::Linear),
+  ]);
+
+
+  println!("value at 0, 0, 0: {:?}", nested.sample(0.1).unwrap().sample(0.1).unwrap().sample(0.1));
+  println!("value at 0, 0, 1: {:?}", nested.sample(0.1).unwrap().sample(0.1).unwrap().sample(0.9));
+  println!("value at 0, 1, 0: {:?}", nested.sample(0.1).unwrap().sample(0.9).unwrap().sample(0.1));
+  println!("value at 0, 1, 1: {:?}", nested.sample(0.1).unwrap().sample(0.9).unwrap().sample(0.9));
+
+  println!("value at 1, 0, 0: {:?}", nested.sample(0.9).unwrap().sample(0.1).unwrap().sample(0.1));
+  println!("value at 1, 0, 1: {:?}", nested.sample(0.9).unwrap().sample(0.1).unwrap().sample(0.9));
+  println!("value at 1, 1, 0: {:?}", nested.sample(0.9).unwrap().sample(0.9).unwrap().sample(0.1));
+  println!("value at 1, 1, 1: {:?}", nested.sample(0.9).unwrap().sample(0.9).unwrap().sample(0.9));
+
 }
 
